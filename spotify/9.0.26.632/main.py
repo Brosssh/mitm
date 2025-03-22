6f078f09a9f1a863069af6793dc3d06c222f87d1
brosssh <tiabroch@gmail.com>
2025-03-22 18:34:31 +0100

    Removing big ads in home page

--- spotify/9.0.26.632/main.py
+++ spotify/9.0.26.632/main.py
@@ -3,6 +3,7 @@ import io
 from mitmproxy import http, ctx
 from patches.signature_spoof import *
 from patches.spoof_trial import *
+from patches.misc import *
 
 
 def request(flow: http.HTTPFlow) -> None:
@@ -11,5 +12,8 @@ def request(flow: http.HTTPFlow) -> None:
 
 def response(flow: http.HTTPFlow) -> None:
 
-    if "bootstrap" in flow.request.pretty_url:
-        spoof_trial(flow)
+    #if "bootstrap" in flow.request.pretty_url or "user-customization-servi" in flow.request.pretty_url:
+    #    spoof_trial(flow)
+
+    if "eager" in flow.request.pretty_url:
+        remove_front_ads(flow)
---
+++ spotify/9.0.26.632/patches/misc.py
@@ -0,0 +1,16 @@
+import json
+import blackboxprotobuf
+from mitmproxy import ctx, http
+
+
+def remove_front_ads(flow: http.HTTPFlow):
+        message, typedef = blackboxprotobuf.protobuf_to_json(flow.response.content)
+
+        message_json = json.loads(message)
+        ctx.log.info(type(message_json))
+        ctx.log.info(message_json["1"]["1"][1])
+        del message_json["1"]["1"][1]
+
+        message = json.dumps(message_json)
+        
+        flow.response.content = blackboxprotobuf.protobuf_from_json(message, typedef)
